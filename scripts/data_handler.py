0443b0ade28bdd6dbc85dc0789b5673549056f4c
polarcube1 <polarcube1@gmail.com>
2022-05-04 19:34:50 -0400

    Added Global Variables

--- scripts/data_handler.py
+++ scripts/data_handler.py
@@ -5,13 +5,24 @@ from os.path import exists
 # External Scripts
 from components import Student
 
+
 student = Student('Nicolas', 'Gatien', '+13439985454')
 path = f'students/{student.first_name}_{student.last_name}.json'
+
+global_variables = json.load(open('scripts\global_variables.json'))
+
+def Increase_Number_Of_Students():
+    global_variables['number_of_students'] += 1
+    
+    global_variables_json = json.dumps(global_variables)
+
+    with open('scripts\global_variables.json', 'w') as outfile:
+        outfile.write(global_variables_json)
 
 def Create_JSON_File_For_Student():
     # Create a Dictionary With All Information
     student_dict = {
-        "ID": 0,
+        "ID": global_variables['number_of_students'],
         "first_name": student.first_name,
         "last_name": student.last_name,
         "phone_number": student.phone_number,
@@ -33,6 +44,9 @@ def Create_JSON_File_For_Student():
         # Write JSON File in students folder, name the file after the student
         with open(f'students/{student.first_name}_{student.last_name}.json', 'w') as outfile:
             outfile.write(student_json)
+        
+        # Log Student Into Global Variables
+        Increase_Number_Of_Students()
 
         # Debug
         print(f"📰 Create File For {student.first_name} {student.last_name}")
@@ -40,6 +54,7 @@ def Create_JSON_File_For_Student():
 
 Create_JSON_File_For_Student()
 
+# Up Module By 1
 file = open(path)
 python_dictionary = json.load(file)
 
