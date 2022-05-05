542dd840a50ea84e2df216dbee41a4de2551f425
polarcube1 <polarcube1@gmail.com>
2022-05-05 09:45:10 -0400

    Send Message to All Stored Students

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 # Libraries
 from twilio.rest import Client
+import os
+import json
 
 # Other Scripts
 import keys.twilio_keys
@@ -7,20 +9,21 @@ from components import Student
 import data_handler
 
 client = Client(keys.twilio_keys.account_sid, keys.twilio_keys.auth_token) # Twilio Client
-students = [Student('Nicolas', 'Gatien', '+13439985454')] # Student List
+directory = 'data\students' # Students Directory
 
 data_handler.Download_Student_Data()
 data_handler.Export_Student_Data_In_JSON()
 
-# Loop Through Each Student In List
-'''for student in students:
+for filename in os.scandir(directory):
+    if filename.is_file():
+        file = json.load(open(filename.path))
 
-    # Construct Message
-    message = client.messages.create(
-        body = f"Welcome, {student.first_name}! This is your first lesson with 🧱 Foundations \n 🔗 Press This Link To Continue To Your First Lesson: https://www.youtube.com/watch?v=iik25wqIuFo",
-        from_ = keys.twilio_keys.twilio_number,
-        to = student.phone_number
-    )
+        # Construct Message
+        message = client.messages.create(
+            body = f"Welcome, {file['first_name']}! This is your first lesson with 🧱 Foundations \n 🔗 Press This Link To Continue To Your First Lesson: https://www.youtube.com/watch?v=iik25wqIuFo",
+            from_ = keys.twilio_keys.twilio_number,
+            to = file['phone_number']
+        )
 
-    # Debug Message
-    print(f"Sent: {message.body} to {student.phone_number}")'''
+        # Debug Message
+        print(f"Sent: {message.body} to {file['phone_number']}")
